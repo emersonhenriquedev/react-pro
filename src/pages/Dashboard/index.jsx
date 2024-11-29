dbc4f0cd7dc36c5a492570cd3bf988eb03ea3ec6
Emerson Henrique <emerson.hm@outlook.com>
2024-11-28 21:36:50 -0300

    list users page integration api

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,64 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import httpClient from "../../services/axios";
 
 export default function Dashboard() {
-  const users = [
-    {
-      id: 1,
-      name: "João da Silva",
-      email: "joao@gmail.com",
-      created_at: "2024-11-19",
-      updated_at: "2024-11-19",
-    },
-    {
-      id: 2,
-      name: "Maria da Silva",
-      email: "maria@gmail.com",
-      created_at: "2024-11-19",
-      updated_at: "2024-11-19",
-    },
-    {
-      id: 3,
-      name: "João da Silva",
-      email: "joao@gmail.com",
-      created_at: "2024-11-19",
-      updated_at: "2024-11-19",
-    },
-    {
-      id: 4,
-      name: "Maria da Silva",
-      email: "maria@gmail.com",
-      created_at: "2024-11-19",
-      updated_at: "2024-11-19",
-    },
-    {
-      id: 5,
-      name: "João da Silva",
-      email: "joao@gmail.com",
-      created_at: "2024-11-19",
-      updated_at: "2024-11-19",
-    },
-    {
-      id: 6,
-      name: "Maria da Silva",
-      email: "maria@gmail.com",
-      created_at: "2024-11-19",
-      updated_at: "2024-11-19",
-    },
-  ];
-
   const [page, setPage] = useState(1);
-
+  const [users, setUsers] = useState([]);
+  const [totalPages, setTotalPage] = useState(1);
   const perPage = 2;
-  const totalPages = Math.ceil(users.length / perPage);
 
-  function handleShowMore() {
-    setPage(page + 1);
+  function changePage(pageNumber) {
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setPage(pageNumber);
+    }
   }
+
+  async function getUsers() {
+    const token = localStorage.getItem("token");
+    const headers = { headers: { Authorization: `Bearer ${token}` } };
+    const response = await httpClient.get(
+      `/users?page=${page}&perPage=${perPage}`,
+      headers
+    );
+    setUsers(response.data.users);
+    setTotalPage(response.data.numberOfPages);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [page]);
 
   return (
     <div className="custom-container">
-      <div className="flex justify-between items-center">
+      <div className="flex items-center justify-between">
         <h1 className="text-2xl font-medium">Usuários</h1>
         <Link to="/dashboard/users/create" className="btn text-primary">
           Novo Usuário
@@ -71,36 +44,50 @@ export default function Dashboard() {
               <th>Id</th>
               <th>Nome</th>
               <th>E-mail</th>
+              <th>Função</th>
               <th>Criação</th>
               <th>Atualização</th>
             </tr>
           </thead>
           <tbody>
-            {users.slice(0, perPage * page).map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.created_at}</td>
-                <td>{user.updated_at}</td>
-                <td>
-                  <Link to={`/dashboard/users/edit/${user.id}`} className="text-blue-400">Editar</Link>
-                </td>
-              </tr>
-            ))}
+            {users &&
+              users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role.name}</td>
+                  <td>{user.createdAt}</td>
+                  <td>{user.updatedAt}</td>
+                  <td>
+                    <Link
+                      to={`/dashboard/users/edit/${user.id}`}
+                      className="text-blue-400"
+                    >
+                      Editar
+                    </Link>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
-      <div className="flex justify-center">
-        {page < totalPages && (
+      <div className="flex justify-center mt-6">
+        <div className="join">
           <button
-            onClick={handleShowMore}
-            type="button"
-            className="mt-4 text-primary"
+            onClick={() => changePage(page - 1)}
+            className="join-item btn"
           >
-            Mostrar mais
+            «
           </button>
-        )}
+          <button className="join-item btn">Página {page}</button>
+          <button
+            onClick={() => changePage(page + 1)}
+            className="join-item btn"
+          >
+            »
+          </button>
+        </div>
       </div>
     </div>
   );
