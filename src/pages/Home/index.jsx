10d77dfe4c35aa5097848de4ff3b726dce8a4cad
Emerson Henrique <emerson.hm@outlook.com>
2024-12-11 23:29:04 -0300

    home search bar integration api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,32 +2,29 @@ import { useState, useEffect } from "react";
 
 import ProductCard from "./components/ProductCard";
 import SearchBar from "./components/SearchBar";
-import { products as productsList } from "./consts";
 import httpClient from "../../services/axios";
 import Pagination from "../../components/Pagination";
 
 export default function Home() {
-  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchedProducts, setSearchedProducts] = useState([]);
 
   const perPage = 10;
 
-  function onSearchHandler(value) {
+  async function onSearchHandler(value) {
     if (value) {
-      setFilteredProducts(
-        productsList.filter((product) =>
-          product.name.toLowerCase().includes(value.toLowerCase())
-        )
-      );
-    } else {
-      setFilteredProducts(productsList);
+      setSearchValue(value);
+      const { data } = await httpClient.get(`/products/search/${value}`);
+      setSearchedProducts(data);
     }
   }
 
   function onChangeSearchHandler(value) {
     if (!value) {
-      setFilteredProducts([]);
+      setSearchValue("");
     }
   }
 
@@ -37,19 +34,20 @@ export default function Home() {
     }
   }
 
+  async function fetchProducts() {
+    try {
+      const params = { page, perPage };
+      const { data } = await httpClient.get("/products", {
+        params,
+      });
+      setProducts(data.products);
+      setTotalPage(data.numberOfPages);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const params = { page, perPage };
-        const { data } = await httpClient.get("/products", {
-          params,
-        });
-        setFilteredProducts(data.products);
-        setTotalPage(data.numberOfPages);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchProducts();
   }, [page]);
 
@@ -63,21 +61,30 @@ export default function Home() {
         <div className="mt-10">
           <h1 className="text-4xl font-medium">Produtos</h1>
           <ul className="grid grid-cols-4 gap-6 mt-6">
-            {filteredProducts.map((product) => (
-              <li key={product.id}>
-                <ProductCard product={product} />
-              </li>
-            ))}
+            {searchValue
+              ? searchedProducts.map((product) => (
+                  <li key={product.id}>
+                    <ProductCard product={product} />
+                  </li>
+                ))
+              : products.map((product) => (
+                  <li key={product.id}>
+                    <ProductCard product={product} />
+                  </li>
+                ))}
           </ul>
-          <div className="flex justify-center mt-3">
-            <Pagination
-              onClickNext={() => changePage(page + 1)}
-              onClickPrevious={() => changePage(page - 1)}
-              page={page}
-              isPreviousDisabled={page === 1}
-              isNextDisabled={page + 1 > totalPages}
-            />
-          </div>
+
+          {searchValue ? null : (
+            <div className="flex justify-center mt-3">
+              <Pagination
+                onClickNext={() => changePage(page + 1)}
+                onClickPrevious={() => changePage(page - 1)}
+                page={page}
+                isPreviousDisabled={page === 1}
+                isNextDisabled={page + 1 > totalPages}
+              />
+            </div>
+          )}
         </div>
       </main>
     </div>
