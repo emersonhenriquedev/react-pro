112562c3c2e33ea69408afbd74181460a01297b8
Emerson Henrique <emerson.hm@outlook.com>
2025-01-25 16:51:07 -0300

    clear layout code

--- src/pages/Dashboard/Layout/index.jsx
+++ src/pages/Dashboard/Layout/index.jsx
@@ -1,17 +1,7 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import { useContext, useEffect } from "react";
-import { authContext } from "../../../contexts/authContext";
 
 export default function Layout() {
-  const navigate = useNavigate();
-  const { user } = useContext(authContext);
-  // useEffect(() => {
-  //   console.log(user);
-  //   if (!user) {
-  //     navigate("/");
-  //   }
-  // }, [user, navigate]);
 
   return (
     <>
